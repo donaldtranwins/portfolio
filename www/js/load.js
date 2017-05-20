727bd940180cf08a9f2f85c0310ba02c5db48343
Donald Tran <tran.donald@gmail.com>
2017-05-19 21:07:39 -0700

    Added Phone/Email append functions

--- www/js/load.js
+++ www/js/load.js
@@ -1,25 +1,38 @@
-$(document).ready(init);
+$(document).ready(initialize);
 
-function init(){
+function initialize(){
     appendEmail();
     appendPhone();
     allowCollapseHamburger();
 }
 
 function appendEmail(){
-
+    let e = "tra";
+    e += "ndo";
+    e += "nald";
+    e += "@gm";
+    e += "ail";
+    e += ".c";
+    e += "om";
+    $('#e').text(e).attr('href', `mailto:${e}?subject=Position%20Available:%20&body=Hi%20Donald,%0A%0A%20I%20read%20your%20resume,%20love%20it.%20%20I%20wanted%20to%20reach%20out%20about%20an%20opportunity%20we%20have%20for%20you:%0A%0A%0AThanks,%0A%0A`)
 }
 
 function appendPhone(){
-
+    let p = '71';
+    p += '4-';
+    p += '39';
+    p += '8-';
+    p += '62';
+    p += '69';
+    $('#p').text(p);
 }
 
 function allowCollapseHamburger() {
     $(document).click(function (event) {
-        var clickover = $(event.target);
-        var hamburger = $(".mobile-nav");
-        var opened = hamburger.hasClass("active");
-        if (opened === true && !clickover.hasClass("clearlist") && !clickover.hasClass("fa-bars") ) {
+        const clicktarget = $(event.target);
+        const hamburger = $(".mobile-nav");
+        const opened = hamburger.hasClass("active");
+        if (opened && !clicktarget.hasClass("clearlist") && !clicktarget.hasClass("fa-bars") ) {
             $(".mobile-nav").click();
         }
     });
