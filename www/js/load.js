c2e3e1d0eaa54e67d7c28d1a56f498072672c5d5
Donald J Tran <tran.donald@gmail.com>
2017-05-27 16:58:40 -0700

    Edited logos in paint.net

--- www/js/load.js
+++ www/js/load.js
@@ -8,13 +8,13 @@ function initialize(){
 }
 
 function appendEmail(){
-    let e = "tra";
-    e += "ndo";
-    e += "nald";
-    e += "@gm";
-    e += "ail";
-    e += ".c";
-    e += "om";
+    let e = "don";
+    e += "ald";
+    e += "jtr";
+    e += "an@";
+    e += "gma";
+    e += "il.";
+    e += "com";
     let m = 'mai';
     m += 'lto:';
     $('#e').text(e).attr('href', `${m+=e}?subject=Position%20Available&#58;%20&body=Hi%20Donald&#44;%0A%0A%20I%20read%20your%20resume&#44;%20love%20it&#46;%20%20I%20wanted%20to%20reach%20out%20about%20an%20opportunity%20we%20have%20for%20you&#58;%0A%0A%0AThanks&#44;%0A%0A`)
@@ -23,7 +23,7 @@ function appendEmail(){
 function appendPhone(){
     let p = '71';
     p += '4-';
-    p += '39';
+    p += '24';
     p += '8-';
     p += '62';
     p += '69';
@@ -45,4 +45,12 @@ function allowCollapseHamburger() {
             hamburger.click();
         }
     });
+}
+function applyFlipHandler(){
+    const $card = $('.card');
+    $card.on('click',function(){
+        if (!$card.hasClass('flipped')){
+            $card.addClass('flipped');
+        }
+    })
 }
